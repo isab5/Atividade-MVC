4a45a37bdd0184c89f8f0fe5ac612a3399e66f1e
Isabella Borin <158210136+isab5@users.noreply.github.com>
2025-02-04 11:20:23 -0300

    refactor: adjustment constructor plays

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,12 +1,13 @@
 const { v4: uuid4 } = require("uuid");
 
 class Movie {
-    constructor(title, director, gender, duration) {
+    constructor(title, director, gender, duration, plays = 0) {
         this.id = uuid4();
         this.title = title;
         this.director = director;
         this.gender = gender;
         this.duration = duration;
+        this.plays = plays;
     }
 
     play() {
