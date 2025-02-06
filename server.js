6cfcd1472b816e6f62c539ff06046085596489fe
Isabella <isabellaborin.tds1.senai@gmail.com>
2025-02-05 22:39:07 -0300

    feat: create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const movieRoutes = require("./src/routes/movieRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", movieRoutes);
+
+app.get("/", (req, res) => {
+    res.send("EU AMO O HOMEM DE FERRO!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
