4eb2f694e3dddd94f14f5eb85a20d2bdc43afcb4
Isabella <isabellaborin.tds1.senai@gmail.com>
2025-02-05 22:19:46 -0300

    feat: create folder Routes and create file movieRoutes

---
+++ src/routes/movieRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const movieController = require("../controllers/movieController");
+
+router.get("/movies", movieController.getAllMovies);
+router.post("/movies", movieController.addMovie);
+router.put("/movies/:id", movieController.updateMovie);
+router.delete("/movies/:id", movieController.deleteMovie);
+router.get("/movies/:top10", movieController.getTop10Movies);
+router.get("/movies/:id", movieController.getMovieById);
+
+module.exports = router;
